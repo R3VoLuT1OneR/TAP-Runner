2021df3ebbb2b5f0cd691e3b9c66806c35edea40
Pavel Zhytomirsky <r3volut1oner@gmail.com>
2012-07-09 10:11:19 +0300

    Coerece from arrayref

--- lib/TAP/Runner/Option.pm
+++ lib/TAP/Runner/Option.pm
@@ -1,6 +1,14 @@
 package TAP::Runner::Option;
 # ABSTRACT: Option object
 use Moose;
+use Moose::Util::TypeConstraints;
+
+subtype 'ArrayRef::' . __PACKAGE__,
+    as 'ArrayRef[' . __PACKAGE__ . ']';
+
+coerce 'ArrayRef::' . __PACKAGE__,
+    from 'ArrayRef[HashRef]',
+    via { [ map { __PACKAGE__->new($_) } @{$_} ] };
 
 has name          => (
     is            => 'ro',
