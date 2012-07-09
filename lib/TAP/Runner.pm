dac8fa3d0ae6289af02e1fad06c07558025579f7
Pavel Zhytomirsky <r3volut1oner@gmail.com>
2012-07-09 10:45:04 +0300

    Refactor to use new objects

--- lib/TAP/Runner.pm
+++ lib/TAP/Runner.pm
@@ -23,63 +23,16 @@ has harness_args  => (
 
 has tests         => (
     is            => 'rw',
-    isa           => 'ArrayRef[HashRef]',
+    isa           => 'ArrayRef::TAP::Runner::Test',
+    coerce        => 1,
     required      => 1,
 );
-
-has _tap_tests    => (
-    is            => 'rw',
-    isa           => 'ArrayRef[TAP::Runner::Test]',
-    lazy_build    => 1,
-);
-
-sub _build__tap_tests {
-    my $self  = shift;
-    my @tests = ();
-
-    foreach my $test ( @{ $self->tests } ) {
-
-        if ( defined $test->{options} && ref $test->{options} eq 'HASH' ) {
-
-            # Build arrays of all options and add option name to each value
-            my @all_options_array = ();
-            while ( my ( $option_name, $values ) = each %{ $test->{options} } ) {
-                push @all_options_array,  [ map{ [ $option_name, $_ ] } @$values ];
-            }
-
-            # Make cartesian multiplication with all options
-            foreach ( cartesian { @_ } @all_options_array ) {
-
-                # Build correct test args array
-                # First element this is option name and second option value
-                my @test_opts  = ( map{ ($_->[0],$_->[1]) } @$_ );
-                my @test_args  = ( @{$test->{args}}, @test_opts );
-                my $test_alias = $test->{alias}  .' '. join(' ',@test_opts);
-
-                push @tests, TAP::Runner::Test->new({
-                    file  => $test->{file},
-                    alias => $test_alias,
-                    args  => \@test_args,
-                });
-
-            }
-
-        } else {
-
-            push @tests, TAP::Runner::Test->new($test);
-
-        }
-
-    }
-
-    \@tests;
-}
 
 sub run {
     my $self          = shift;
     my $harness_class = $self->harness_class;
-    my $harness_args  = $self->get_harness_args;
-    my @harness_tests = $self->get_harness_tests;
+    my $harness_args  = $self->_get_harness_args;
+    my @harness_tests = $self->_get_harness_tests;
 
     # Load harness class
     eval "require $harness_class";
@@ -94,18 +47,24 @@ sub run {
     $harness->runtests( @harness_tests );
 }
 
-sub get_harness_args {
+# Harness args with building test arguments
+sub _get_harness_args {
     my $self      = shift;
-    my %test_args = map { ( $_->alias, $_->args ) } @{$self->_tap_tests};
 
-    $self->harness_args->{test_args} = \%test_args;
+    # Build tests args hash ref
+    $self->harness_args->{test_args} = {
+        map { ( $_->{alias}, $_->{args} ) }
+        map { @{ $_->harness_tests }      } @{ $self->tests } };
+
     $self->harness_args;
 }
 
-sub get_harness_tests {
+# Return array of tests to run with it aliases
+sub _get_harness_tests {
     my $self  = shift;
 
-    map{ [ $_->file, $_->alias ] } @{ $self->_tap_tests };
+    map{ [ $_->{file}, $_->{alias} ] }
+    map{ @{ $_->harness_test }       } @{ $self->tests };
 }
 
 no Moose;
